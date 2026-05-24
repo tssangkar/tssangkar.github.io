e14bb4f3976207e8daa4670abdf92ddf9e3d4b28
Teguh S <teguh-s@localhost>
2026-05-24 21:08:36 +0700

    chore: remove migration source folder

--- kasir-ts-sangkar-v2/src/data/products.ts
+++
@@ -1,9 +0,0 @@
-import { Product } from '../types';
-
-export const INITIAL_PRODUCTS: Product[] = [];
-
-export const SHEET_API_URL =
-  "https://script.google.com/macros/s/AKfycbxE4eMX3_TJlSkXitw2txzNv1R_AUMUarPFbatelhUgLw6V6IStSrys9C8lEZUhn-ZSgQ/exec";
-
-export const SHEET_API_URL_PESANAN =
-  "https://script.google.com/macros/s/AKfycbxE4eMX3_TJlSkXitw2txzNv1R_AUMUarPFbatelhUgLw6V6IStSrys9C8lEZUhn-ZSgQ/exec";
